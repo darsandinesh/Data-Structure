f2300e7a320e2601462482fff16242856b173061
Darsan Dinesh <darsandinesh100@gmail.com>
2024-03-02 18:16:48 +0530

    bubbleSort updated

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,17 +1,18 @@
-function bubbleSort(arr){
-    for(let i=0;i<arr.length;i++){
-        let change = 0
-        if(arr[i]>arr[i+1]){
-            temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1]=temp
-            change =1
+function bubbleSort(arr) {
+    let change = 0
+    for (let i = 0; i < arr.length ; i++) {
+        if (arr[i] > arr[i + 1]) {
+            let temp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i + 1] = temp
+            change = 1
         }
-        if((i == arr.length-1) & change ==1){
-            i=0
+        if ((i == arr.length - 2) & change == 1) {
+            change=0
+            i = -1
         }
     }
     return arr
 }
 
-console.log(bubbleSort([5,3,7,1,5,2]))
+console.log(bubbleSort([5, 3, 7, 1, 5, 2]))
